18e21ac0df90672cc8ee468cd8aaf12f53fa5bee
App <ramsourav82@gmail.com>
2024-08-26 14:38:34 +0600

    whatsapp design

---
+++ app/src/main/java/com/example/souravram/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.souravram
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.souravram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn1.setOnClickListener {
+
+            Toast.makeText(this, "Successfully Click", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
